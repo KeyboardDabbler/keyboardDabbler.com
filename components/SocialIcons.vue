4462392eb4660fd8fd78865a8b8e7abc37f07138
keyboarddabbler <keyboarddabbler@gmail.com>
2023-04-18 19:32:34 +1200

    fix: social links

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    <NuxtLink target="_blank" href="https://github.com/harlan-zw" title="Visit my Gitea" class="group">
+    <NuxtLink target="_blank" href="https://gitea.keyboarddabbler.com" title="Visit my Gitea" class="group">
       <i-line-md-github class="icon icon--off" alt="Gitea Logo" />
       <i-line-md:github-twotone class="icon icon--on" alt="Gitea Logo" />
     </NuxtLink>
-    <NuxtLink target="_blank" href="https://discord.com/invite/5jDAMswWwX" title="Email Me" class="group">
+    <NuxtLink target="_blank" href="mailto:keyboarddabbler@gmail.com" title="Email Me" class="group">
       <i-ic:sharp-mail-outline class="icon icon--off" alt="Email Logo" />
       <i-ic:twotone-mail class="icon icon--on" alt="Email Logo" />
-    </NuxtLink>
-    <NuxtLink target="_blank" href="https://discord.com/invite/5jDAMswWwX" title="Visit my Twitter" class="group">
-      <i-line-md:twitter class="icon icon--off" alt="Twitter Logo" />
-      <i-line-md:twitter-twotone class="icon icon--on" alt="Twitter Logo" />
     </NuxtLink>
   </div>
 </template>
