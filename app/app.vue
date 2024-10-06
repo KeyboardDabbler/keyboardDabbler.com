a1d79d13b05c6ac77ebd46392f8d57f797808345
keyboarddabbler <keyboarddabbler@gmail.com>
2024-10-06 16:29:03 +1300

    style: add useHead

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: '#111827' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
 </script>
 
 <template>
-    <div>
-      <NuxtLoadingIndicator />
-  
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-  
-    </div>
-  </template>
+  <div>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
