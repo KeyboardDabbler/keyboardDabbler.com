cc7c1e946406372e387506374af3df9a81b41375
keyboarddabbler <keyboarddabbler@gmail.com>
2024-10-06 16:56:04 +1300

    feat: add nuxt-og-image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxt/ui", 
-    "@nuxt/eslint", 
-    "@nuxt/content"
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxt/content",
+    'nuxt-og-image'
   ],
 
   colorMode: {
