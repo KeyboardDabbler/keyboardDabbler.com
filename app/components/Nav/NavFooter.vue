7aec8d8e5f1de5ac155150ad4b9a36e086d5f08f
keyboarddabbler <keyboarddabbler@gmail.com>
2024-12-13 14:02:52 +1300

    fix: remove <script> from footer

---
+++ app/components/Nav/NavFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="px-5 sm:px-7 md:px-10 text-center text-gray-400 text-sm my-5">
+    <div class="flex justify-center mb-3">
+      <SocialIcons />
+    </div>
+    <p class="my-3">
+      Made with ❤️ by <span class="text-yellow-1 font-bold">keyboardDabbler</span> | © 2023-Present
+    </p>
+  </footer>
+</template>
