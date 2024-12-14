3ffc5ced684092265ab0b317ab9b0287429a9ace
keyboarddabbler <keyboarddabbler@gmail.com>
2024-12-14 14:04:56 +1300

    feat: add socialIcon to header

---
+++ app/components/Nav/NavHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UHeader>
+    <template #right>
+      <SocialIcons class="text-gray-500 hidden lg:flex md:(space-x-2) space-x-1 " />
+    </template>
+  </UHeader>
+</template>
