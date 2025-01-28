3fc7fe68eef2eb4506612f190f1a327d83fdbbbb
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-28 18:37:30 +1300

    Refactor: index

---
+++ app/components/content/Twocol.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="md:max-w-8xl md:pb-20 md:px-8 sm:pb-16 mx-auto max-w-full px-4 py-7 pb-10 max-w-100ch">
+    <div class="prose prose-gray dark:prose-invert mb-8 md:mb-12">
+      <div class="lg:grid lg:grid-cols-12 lg:gap-20 relative">
+        <div class="col-span-7">
+          <slot name="left" />
+        </div>
+        <div class="col-span-5 text-center">
+          <div class="sticky top-35">
+            <slot name="right" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-
+const home = await queryCollection('home').path('/').first()
+if (!home.body) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
 </script>
 
 <template>
-  <div class="" />
+  <UContainer>
+    <UPage>
+      <ContentRenderer :value="home" />
+    </UPage>
+  </UContainer>
 </template>
-
-<style scoped>
-
-</style>
