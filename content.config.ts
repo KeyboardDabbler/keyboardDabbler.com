56dbb67edf350a3ab8c587031a066ec7609988ed
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-28 18:36:22 +1300

    Fix: add home collection

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,14 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    home: defineCollection({
       type: 'page',
-      source: '**/*.md'
+      source: 'index.md',
+      schema: z.object({
+        title: z.string(),
+        description: z.string()
+      })
     })
   }
 })
