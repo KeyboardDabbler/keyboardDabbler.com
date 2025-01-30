d9f8338a838a498d11c752bb324122e6875bc1d3
keyboarddabbler <keyboarddabbler@gmail.com>
2025-01-30 18:24:54 +1300

    Feat: blog page layout

--- content.config.ts
+++ content.config.ts
@@ -18,6 +18,31 @@ export default defineContentConfig({
         description: z.string(),
         tags: z.array(z.string()).optional()
       })
+    }),
+    blogData: defineCollection({
+      type: 'data',
+      source: 'blog.yml',
+      schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        posts: z.array(
+          z.object({
+            title: z.string(),
+            path: z.string(),
+            date: z.string()
+          })
+        )
+      })
+    }),
+    blog: defineCollection({
+      type: 'page',
+      source: 'blog/**/*.md',
+      schema: z.object({
+        title: z.string(),
+        date: z.string(),
+        description: z.string().optional(),
+        tags: z.array(z.string()).optional()
+      })
     })
   }
 })
