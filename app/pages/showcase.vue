ee2c55688fe8546821fdd9730f548e64de4961f6
keyboarddabbler <keyboarddabbler@gmail.com>
2025-03-23 12:45:44 +1300

    Temporary commit for migration

---
+++ app/pages/showcase.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const page = await queryCollection('showcaseData').first()
+
+const isYearly = ref('0')
+
+const items = ref([
+  {
+    label: 'Web Apps',
+    icon: 'carbon:application-web',
+    value: '0'
+  },
+  {
+    label: 'GFX',
+    icon: 'material-symbols-light:design-services-outline-rounded',
+    value: '1'
+  }
+])
+</script>
+
+<template>
+  <div v-if="page">
+    <UPageHero v-bind="page.hero">
+      <template #links>
+        <UTabs
+          v-model="isYearly"
+          :items="items"
+          color="primary"
+          class="w-72"
+          :ui="{ list: 'rounded-full', indicator: 'rounded-full' }"
+        />
+      </template>
+    </UPageHero>
+  </div>
+</template>
